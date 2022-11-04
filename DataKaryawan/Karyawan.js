6b1ce772ab88851c2caf14280e4e8b96101ab4ee
M0kha666 <hfdhardcore123@gmail.com>
2022-11-04 11:25:32 +0700

    Aplikasi Data Karyawan

---
+++ DataKaryawan/Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan = [
+    {id:'1', nama:'Mochammad Khafidz', jabatan:'CEO Mokha Tech. Co., Ltd', //Chief Executive Officer
+    email:'mochammad_20520005@stimata.ac.id', telp:'0895359629199',
+    gaji:'45.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'Steve Musk', jabatan:'CMO Mokha Tech. Co., Ltd', //Chief Marketing Officer
+    email:'imnoteslapple@stimata.ac.id', telp:'0815259639177',
+    gaji:'30.049.900', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'3', nama:'Mark Gates', jabatan:'COO Mokha Tech. Co., Ltd', //Chief Operating Officer
+    email:'facebookisMICRO@stimata.ac.id', telp:'0826159326179',
+    gaji:'20.075.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'4', nama:'Bill Zuckerberg', jabatan:'CFO Mokha Tech. Co., Ltd', //Chief Financial Officer
+    email:'softlikeab00k@stimata.ac.id', telp:'0855679622431',
+    gaji:'15.005.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'5', nama:'Elon Jobs', jabatan:'CTO Mokha Tech. Co., Ltd', //Chief Technology Officer
+    email:'electricMini12@stimata.ac.id', telp:'0874329676897',
+    gaji:'10.750.000', foto:'https://placeimg.com/100/100/grayscale'},
+];
+
+export default karyawan;
